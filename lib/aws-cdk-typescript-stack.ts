c1b1e6e1c495996d0f973fdd7dfe171fd84e5662
Suraj Dabadi <shadow-dev@sooraz-mac-pro.local>
2023-08-12 17:24:35 +0545

    Created S3 buckets with Three different construct levels

--- lib/aws-cdk-typescript-stack.ts
+++ lib/aws-cdk-typescript-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,9 +9,36 @@ export class AwsCdkTypescriptStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTypescriptQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Creting S3 bucket Three ways 
+    // Type L1
+    new CfnBucket(this,'MyL1Bucket',{
+    lifecycleConfiguration:{
+      rules:[{
+        expirationInDays:4,
+        status: 'Enabled'
+      }]}
+    })
+
+    // Type L2
+    new Bucket(this,'MyL2Bucket',{
+      lifecycleRules: [{
+        expiration: cdk.Duration.days(4)
+      }]
+    })
+    
+
+    new l3Bucket(this,'MyL3Bucket',5)
+  }
+}
+
+class l3Bucket extends Construct{
+  constructor(scope:Construct, id:string,expiration:number){
+    super(scope,id)
+
+    new Bucket(this,id,{
+      lifecycleRules:[{
+        expiration:cdk.Duration.days(expiration)
+      }]
+    })
   }
 }
