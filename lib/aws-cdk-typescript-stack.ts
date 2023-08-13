c676bcf96fc035ebdbc6128b2b16927365b703ce
Suraj Dabadi <shadow-dev@sooraz-mac-pro.local>
2023-08-13 08:56:21 +0545

    Intrinsic funtion to add Stack Id to bucket name added

--- lib/aws-cdk-typescript-stack.ts
+++ lib/aws-cdk-typescript-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkTypescriptStack extends cdk.Stack {
+
+  private intrinsicSuffix: string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.initializeIntrinsicSuffix()
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTypescriptQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this,"MyBucket",{
+      bucketName:`intrinsic-example-bucket-${this.intrinsicSuffix}`
+    })
+  }
+
+
+  private initializeIntrinsicSuffix(){
+    const shortStackId= cdk.Fn.select(2,cdk.Fn.split('/',this.stackId))
+    this.intrinsicSuffix = cdk.Fn.select(4,cdk.Fn.split('/',this.stackId))
   }
 }
